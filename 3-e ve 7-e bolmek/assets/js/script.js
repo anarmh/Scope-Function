cea77666a8375ffa25f142bab722ad8bd268e3ac
anarmh <anarmh@code.edu.az>
2023-02-08 20:48:31 +0400

    add dividing the number by 3 and 7

---
+++ 3-e ve 7-e bolmek/assets/js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+
+let n;
+
+function dividingTheNumberBy3and7(n){
+    if(n%3==0 && n%7==0){
+        console.log("Bolunur");
+    }
+    else{
+        console.log("Bolunmur");
+    }
+}
+
+dividingTheNumberBy3and7(21);
